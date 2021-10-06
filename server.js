77ba2ca42ae486871ca4f3b1d670338a5d5ff29b
jacksonkasi <jacksonkasi@localhost.localdomain>
2021-10-07 00:55:29 +0530

    make it now

---
+++ server.js
@@ -0,0 +1,33 @@
+let fs = require("fs");
+let express = require("express");
+
+const app = express();
+
+let date = new Date();
+
+let timeStamp = date.toUTCString();
+
+fs.writeFile("date.txt", timeStamp, err => {
+  err && console.error(err);
+});
+
+let time;
+fs.readFile("date.txt", "utf-8", (err, data) => {
+  if (err) {
+    console.error(err);
+  }
+
+  time = data;
+});
+
+setTimeout(() => {
+  getDate();
+}, 100);
+
+function getDate() {
+  app.get("/", (req, res) => {
+    res.send(`<h1> The Current TimeStamp : ${time}</h1>`);
+  });
+}
+
+app.listen(5000);
