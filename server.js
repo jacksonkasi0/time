c93b3271f7f1907b7160029eeefab2c46a7a85b6
jacksonkasi <jacksonkasi@localhost.localdomain>
2021-10-07 01:16:28 +0530

    make it better

--- server.js
+++ server.js
@@ -7,28 +7,28 @@ let date = new Date();
 
 let timeStamp = date.toUTCString();
 
-// fs.writeFile("date.txt", timeStamp, err => {
-//   err && console.error(err);
-// });
-
-// let time;
-// fs.readFile("date.txt", "utf-8", (err, data) => {
-//   if (err) {
-//     console.error(err);
-//   }
-
-//   time = data;
-// });
-
-// setTimeout(() => {
-//   getDate();
-// }, 100);
-
-// function getDate() {
-app.get("/", (req, res) => {
-  res.send(`<h1> The Current TimeStamp : ${timeStamp}</h1>`);
+fs.writeFile("date.txt", timeStamp, err => {
+  err && console.error(err);
 });
-// }
+
+let time;
+fs.readFile("date.txt", "utf-8", (err, data) => {
+  if (err) {
+    console.error(err);
+  }
+
+  time = data;
+});
+
+setTimeout(() => {
+  getDate();
+}, 100);
+
+function getDate() {
+  app.get("/", (req, res) => {
+    res.send(`<h1> The Current TimeStamp : ${time}</h1>`);
+  });
+}
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("server run fine");
