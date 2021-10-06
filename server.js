f1448af654369cde850d3dc7ad12120f572e0daf
jacksonkasi <jacksonkasi@localhost.localdomain>
2021-10-07 01:44:18 +0530

    make it better

--- server.js
+++ server.js
@@ -3,32 +3,30 @@ let express = require("express");
 
 const app = express();
 
-function getDate() {
-  let date = new Date();
+let date = new Date();
 
-  let timeStamp = date.toUTCString();
+let timeStamp = date.toUTCString();
 
-  fs.writeFile("date.txt", timeStamp, err => {
-    err && console.error(err);
-  });
+fs.writeFile("date.txt", timeStamp, err => {
+  err && console.error(err);
+});
 
-  let time;
-  fs.readFile("date.txt", "utf-8", (err, data) => {
-    if (err) {
-      console.error(err);
-    }
-    time = data;
-  });
-
-  setTimeout(() => {
-    getDate();
-  }, 100);
-
-  function getDate() {
-    app.get("/", (req, res) => {
-      res.send(`<h1> The Current TimeStamp : ${time}</h1>`);
-    });
+let time;
+fs.readFile("date.txt", "utf-8", (err, data) => {
+  if (err) {
+    console.error(err);
   }
+  time = data;
+});
+
+setTimeout(() => {
+  getDate();
+}, 100);
+
+function getDate() {
+  app.get("/", (req, res) => {
+    res.send(`<h1> The Current TimeStamp : ${time}</h1>`);
+  });
 }
 
 // window.onload = function() {
